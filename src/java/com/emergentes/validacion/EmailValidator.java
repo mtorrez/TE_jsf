5a0a4b49135c7e8a0b2e89cf0999b546558de33d
Mario Torrez <mtorrezc@gmail.com>
2016-05-16 01:25:27 -0400

    Primer commit

---
+++ src/java/com/emergentes/validacion/EmailValidator.java
@@ -0,0 +1,33 @@
+package com.emergentes.validacion;
+
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+import javax.faces.application.FacesMessage;
+import javax.faces.component.UIComponent;
+import javax.faces.component.html.HtmlInputText;
+import javax.faces.context.FacesContext;
+import javax.faces.validator.FacesValidator; 
+import javax.faces.validator.Validator;
+import javax.faces.validator.ValidatorException;
+
+@FacesValidator("emailValidator")
+public class EmailValidator implements Validator {
+
+    @Override
+    public void validate(FacesContext facesContext, UIComponent uIComponent, Object value) {
+        Pattern pattern = Pattern.compile("\\w+@\\w+\\.\\w+");
+        Matcher matcher = pattern.matcher((CharSequence) value);
+        HtmlInputText htmlInputText = (HtmlInputText) uIComponent;
+        String label;
+        if (htmlInputText.getLabel() == null || htmlInputText.getLabel().trim().equals("")) {
+            label = htmlInputText.getId();
+        }else {
+            label = htmlInputText.getLabel();
+        }
+        if (!matcher.matches())  {
+            FacesMessage facesMessage = new FacesMessage(label + " : Correo no válido");
+            throw new ValidatorException(facesMessage);
+        }
+    }
+}
+
